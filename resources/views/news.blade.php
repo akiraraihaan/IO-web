c920e5344a8f12b2af36f829f521386f1327738d
Akira <raihanakirar@gmail.com>
2024-10-07 19:51:53 +0700

    update script news
    update value script in news page

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
     <p class="text-3xl text-center m-12 font-bold text-[#255a7b]">
-        Nantinya ini adalah halaman NEWS and update
+        Nantinya ini adalah halaman NEWS and updates
     </p>
 </x-layout>
